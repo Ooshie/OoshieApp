fc99ab382afb15523fb75532d9804a84bff4e87e
pjpavon <75351277+pjpavon@users.noreply.github.com>
2021-12-05 13:06:58 -0600

    Update App.js

--- App.js
+++ App.js
@@ -3,17 +3,22 @@ import { createStackNavigator } from "react-navigation-stack";
 import WelcomeScreen from "./src/screens/WelcomeScreen";
 import SignInScreen from "./src/screens/SignInScreen";
 import SignUpScreen from "./src/screens/SignUpScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import TheDeck from "./src/screens/TheDeck";
+import TheCove from "./src/screens/TheCove";
 
 const navigator = createStackNavigator({
-  Home : WelcomeScreen,
+  Home : HomeScreen,
   SignIn : SignInScreen,
-  SignUp : SignUpScreen
+  SignUp : SignUpScreen,
+  Deck: TheDeck,
+  Cove: TheCove,
   
   },
   {
     initialRouteName: "Home",
     defaultNavigationOptions: {
-      title: "I want to die"
+      title: "Ooshie"
     }
   });
 
