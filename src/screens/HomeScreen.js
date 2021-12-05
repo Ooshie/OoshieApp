e47dc9f35d80a3e4d349f9bd3ee030de3b1adc22
pjpavon <75351277+pjpavon@users.noreply.github.com>
2021-12-05 13:03:12 -0600

    Add files via upload

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,54 @@
+import React from "react";
+import {View, Text, StyleSheet, Image, Button, TouchableOpacity } from "react-native";
+
+const HomeScreen = (props) => {
+  return (
+    <View style={styles.container}>
+
+        <Image style = {styles.unoImageStyle} source={require('../../assets/uno.png')} />
+
+        <Text style={styles.text}>Campus Dining!</Text>
+        
+        <TouchableOpacity onPress={function(){props.navigation.navigate("Deck")}}>
+        <Image style = {styles.imageStyle} source={require('../../assets/thedeck.png')} />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={function(){props.navigation.navigate("Cove")}}>
+        <Image style = {styles.imageStyle} source={require('../../assets/thecove.png')} />
+        </TouchableOpacity>
+        
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+    color: "midnightblue",
+    textAlign: "center",
+    fontWeight: "bold",
+    textShadowColor: "blue",
+
+  },
+
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+
+  unoImageStyle: {
+    width: 300,
+    height: 100,
+    resizeMode: 'contain',
+  },
+    
+  imageStyle: {
+    width: 300,
+    height: 220,
+    resizeMode: 'contain',
+  }
+
+});
+
+export default HomeScreen;
