b34ba2d8ba8a89cdd5fc0bf4859d808370a7813a
andyphan7 <78451867+andyphan7@users.noreply.github.com>
2021-12-06 22:42:08 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import WelcomeScreen from "./src/screens/WelcomeScreen";
-import SignInScreen from "./src/screens/SignInScreen";
-import SignUpScreen from "./src/screens/SignUpScreen";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SigninScreen from "./src/screens/SigninScreen";
+import SignupScreen from "./src/screens/SignupScreen";
+import TrackCreateScreen from "./src/screens/TrackCreateScreen";
+import TrackDetailScreen from "./src/screens/TrackDetailScreen";
+import TrackListScreen from './src/screens/TrackListScreen';
+import AccountScreen from "./src/screens/AccountScreen";
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import {Provider as AuthProvider} from './src/context/AuthContext';
+import {setNavigator} from './src/navigationRef';
 import HomeScreen from "./src/screens/HomeScreen";
 import TheDeck from "./src/screens/TheDeck";
 import TheCove from "./src/screens/TheCove";
@@ -11,24 +20,47 @@ import Item from "./src/screens/Item";
 import ItemReuslts from "./src/screens/ItemResults";
 import Moes from "./src/screens/Moes";
 
-const navigator = createStackNavigator({
-  Home : HomeScreen,
-  SignIn : SignInScreen,
-  SignUp : SignUpScreen,
-  Deck: TheDeck,
-  Cove: TheCove,
-  Chick: ChickfilA,
-  Item: Item,
-  ItemReuslts: ItemReuslts,
-  Moe: Moes
-  
+
+
+const switchNavigator = createSwitchNavigator({
+    loginFlow: createStackNavigator({
+      Signup: SignupScreen,
+      Signin: SigninScreen,
+      Deck: TheDeck,
+      Cove: TheCove,
+      Chick: ChickfilA,
+      Item: Item,
+      ItemReuslts: ItemReuslts,
+      Moe: Moes,
+      Home: HomeScreen
+    }),
+    mainFlow: createBottomTabNavigator({
+      trackListFlow: createStackNavigator({
+        TrackList: TrackListScreen,
+        TrackDetail: TrackDetailScreen
+      }),
+      TrackCreate: TrackCreateScreen,
+      Account: AccountScreen
+    })
+});
+
+
+const App = createAppContainer(switchNavigator);
+
+export default () => {
+  return (
+    <AuthProvider>
+      <App ref={(navigator) => {setNavigator(navigator)}}/>
+    </AuthProvider>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  {
-    initialRouteName: "Home",
-    defaultNavigationOptions: {
-      title: "Ooshie"
-    }
-  });
-
-export default createAppContainer(navigator);
-
+});
