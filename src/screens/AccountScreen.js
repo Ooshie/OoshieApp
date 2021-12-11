67a016de0ffcca2e7bdfb2c6da5d412d025209fd
Nick Nguyen <35278403+Ooshie@users.noreply.github.com>
2021-12-10 20:05:36 -0600

    Update AccountScreen.js

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -3,11 +3,11 @@ import {View, StyleSheet, Text, Button} from 'react-native'
 import {Context as AuthContext} from '../context/AuthContext'
 import Spacer from '../components/Spacer'
 
-const AccountScreen = () => {
+const AccountScreen = ({navigation}) => {
     const {signout} = useContext(AuthContext);
 
     return <View>
-        <Text style={{ fontSize: 48}}>Account Screen</Text>
+        <Text style={{ fontSize: 48, alignSelf: "center", marginTop: 50}}>Account</Text>
 
         <Spacer>
             <Button title="Sign Out" onPress = {() => {signout()}} />
